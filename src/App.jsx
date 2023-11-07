93a5f760ea5e27848169431f57d6ccd9afb3f83d
Antonio69999 <antoine.chabeauti@gmail.com>
2023-11-07 14:00:30 +0100

    search a city and displaying the weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,30 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Weather from "./components/Weather/Weather";
 import Search from "./components/Search/Search";
 
 function App() {
-  // const baseUri = process.env.REACT_APP_WEATHER_BASE_URI;
   const apiKey = import.meta.env.VITE_API_KEY;
+  const [currentWeather, setCurrentWeather] = useState(null);
 
-  const [city, setCity] = useState(["Lyon"]);
-  const [data, setData] = useState([]);
-  const [temp, setTemp] = useState([]);
-  const [wind, setWind] = useState([]);
-  const [windDegree, setWindDegree] = useState([]);
-  const [image, setImage] = useState([]);
-  const [forcast, setForcast] = useState(null);
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+    const apiUrlCurrentWeather = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${lat},${lon}&days=3&aqi=no&alerts=no`;
 
-  useEffect(() => {
-    const fetchWeatherData = () => {
-      fetch(
-        `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=3&aqi=no&alerts=no`
-      )
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          console.log(data);
-          setData(data);
-          setCity(data.location.name);
-          setTemp(data.current.temp_c);
-          setWind(data.current.wind_kph);
-          setWindDegree(data.current.wind_degree);
-          setImage(data.current.condition.icon);
-        });
-    };
-    fetchWeatherData();
-  }, [city, temp, wind, windDegree, image]);
-
-  // console.log(data.location.name);
-
-  const handleOnSearchChange = (searchData)  => {
-    console.log(searchData);
-  }
+    fetch(apiUrlCurrentWeather)
+      .then((response) => response.json())
+      .then((weatherResponse) => {
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div className="App">
       <Header />
-      <Search onSearchChange={handleOnSearchChange}></Search>
-      {typeof data.location != "undefined" ? (
-        <Weather
-          city={city}
-          temp={temp}
-          wind={wind}
-          windDegree={windDegree}
-          image={image}
-        />
-      ) : (
-        <div></div>
-      )}
+      <Search onSearchChange={handleOnSearchChange} />
+      <Weather data={currentWeather} />
     </div>
   );
 }
--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,34 +1,32 @@
-import Days from "../Days/Days";
-import "./Weather.css";
+import React from "react";
+import Days from "../Days/Days"
 
-// Import all icons
-import sun from "../../assets/icons/sun.svg";
-import cloudysun from "../../assets/icons/cloudy-sun.svg";
-import cloudy from "../../assets/icons/cloudy.svg";
-import rainy from "../../assets/icons/rainy.svg";
-import snowy from "../../assets/icons/snowy.svg";
-import storm from "../../assets/icons/storm.svg";
-import thunder from "../../assets/icons/thunder.svg";
-import windy from "../../assets/icons/windy.svg";
+function Weather({ data }) {
+  if (!data) {
+    return null; // Return nothing if data is not available yet
+  }
 
-function Weather({ city, temp, wind, windDegree, image }) {
   function handleClickDay(e) {
     console.log(e.target);
     document.querySelector(".clickedDays").classList.remove("clickedDays");
     e.target.classList.add("clickedDays");
   }
+
   return (
     <div className="row">
       <div className="col s12 m6 push-m3">
         <div className="weather card blue-grey darken-1">
           <div className="card-content white-text">
-            <span className="card-title">{city}</span>
+            <span className="card-title">{data.location.name}</span>
             <p>
-              <img src={image} alt={image} />
+              <img
+                src={data.current.condition.icon}
+                alt={data.current.condition.icon}
+              />
             </p>
-            <span className="temperature">{temp}°C</span>
+            <span className="temperature">{data.current.temp_c}°C</span>
             <div className="wind">
-              Vent {wind} km/h ({windDegree}°)
+              Vent {data.current.wind_kph} km/h ({data.current.wind_degree}°)
             </div>
           </div>
           <Days handleClickDay={handleClickDay} />
